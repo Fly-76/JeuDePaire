03e8b61d6996a73dc822f0806179d5d919faa828
Fly-76 <58425266+Fly-76@users.noreply.github.com>
2020-08-31 19:48:00 +0200

    Basic static template HTML/CSS

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 console.log("loaded");
+
+let cards = document.getElementsByClassName("card");
+
+for (let card of cards){
+    card.addEventListener( 'click', function() {
+        card.classList.toggle('is-flipped');
+    });    
+}
